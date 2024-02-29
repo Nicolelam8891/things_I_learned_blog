20da41958d18af08b29319df096145cbf4d62627
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2024-02-29 16:31:11 -0700

    Add blog about toString and parseInt

---
+++ February/03.01.2024.js
@@ -0,0 +1,24 @@
+//I did a leetcode challenge today and found out about toString method and parseInt method. I have used parseInt method prior, but had forgotten about it. 
+
+var minimumSum = function(num) {
+  let numArray = [];
+  num = num.toString(); //2932, changes to string
+  
+  for (let i=0; i < num.length; i++) {
+      numArray.push(parseInt(num[i]))
+  }
+
+  numArray.sort();//[2, 2, 3, 9]
+
+  let new1 = numArray[0].toString() + numArray[2].toString();  
+  //2 + 3 -> 23 (string)
+  let new2 = numArray[1].toString() + numArray[3].toString();
+  //2 + 9 -> 29 (string)
+  
+  return parseInt(new1) + parseInt(new2); //23 + 29 = 52
+  //parseInt changes a string of values into an integer
+};
+
+//What I learned today, if I want to change an integer to a string...I can use the .toString() method
+//If I want to change the string back into an integer, I can use hte .parseInt() method
+//These two methods are very useful for me to learn and remember for these types of math problems!
